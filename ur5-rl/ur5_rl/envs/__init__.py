9db456be59cf30837cf6b1fb688f40de45662aff
JorandG <91953623+JorandG@users.noreply.github.com>
2021-10-08 18:10:57 +0200

    Update __init__.py

--- ur5-rl/ur5_rl/envs/__init__.py
+++ ur5-rl/ur5_rl/envs/__init__.py
@@ -1,7 +1,1 @@
-from gym.envs.registration import register
-
-register(
-    id='ur5reach-v0',
-    entry_point='gym_pybullet.envs:Ur5ReachEnv',
-)
-
+from gym_pybullet.envs.ur5reach_env import Ur5ReachEnv
